4d569cf7e283a701937792946e37e5ed241bf841
CinderZhang <72751738+CinderZhang@users.noreply.github.com>
2021-01-14 10:04:05 -0600

    Create Code and Documents folders
    Code: Python/other code
    Documents: Reading files

---
+++ code/hello.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# %% hello cell for 8
+hello="Welcome 8!!!"
+print(hello)
+
+
+# %% new cell to do sth
+
+n1 = 8
+n2 = 6
+print("Product of ",n1," and ",n2, "is", n1*n2)
+
+# %% new cell--trouble maker
+print(hello,n1*dfasljn2)
+
+# %% one more cell
+
+print("Checked!")
+
+# %% copied from notebook
+# Welcome!
+hello="Welcome 2021!!!"
+print(hello)
+
+
+# %% copied from notebook markdown
+* print("Welcome 2021!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
