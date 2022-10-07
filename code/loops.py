7a206d7932797e282bffe6afd39d9a90ab74aa0e
Cinder Zhang <72751738+CinderZhang@users.noreply.github.com>
2022-10-07 13:47:39 -0500

    update loops

--- code/loops.py
+++ code/loops.py
@@ -1,5 +1,6 @@
 
 # %%
+from codecs import ignore_errors
 import pandas as pd
 import numpy as np 
 import pandas_datareader.data as web 
@@ -15,15 +16,18 @@ import datetime as dt
 #df_stk = web.DataReader('AAPL', 'yahoo', start='2019-09-10', end='2019-10-09')
 
 #%% list of stock tickers
-stk_list = ["AAPL","GOOG","INTC"]
-print(stks)
+stk_list = ["AAPL","GOOG","INTC","F","GM","TSLA","MSFT"]
+print(stk_list)
 
-# %%
-i = 0
+# %% initiate the long format dataframe
+#i = 0
+long_df =pd.DataFrame()
+
+#%% loop through
 for stk in stk_list:
-    print(i)
+    #print(i)
     print(stk)
-    i = i+1
+    #i = i+1
     df_stk = web.DataReader(stk, 'yahoo', start='2019-09-10', end='2019-10-09')
     print(df_stk.head())
     # Save the stock price data
@@ -31,7 +35,12 @@ for stk in stk_list:
     filename = stk + '.csv'
     #print(filename)
     df_stk.to_csv(filename)
-#%% combine all stock price info into one long format data file
-# %%
+    # Alternatively, combine all stock price info into one long format data file
+    df_stk['ticker'] = stk
+    long_df = long_df.append(df_stk)
+    
+    
+# %% save
+long_df.to_csv("Combined_stock_price.csv",index=False)
 
 # %%
