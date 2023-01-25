92a9298a2159b63bf40d4af37804b01c0dc8e6a4
CinderZhang <72751738+CinderZhang@users.noreply.github.com>
2023-01-25 16:11:24 -0600

    Update WRDS.py

--- code/WRDS.py
+++ code/WRDS.py
@@ -2,18 +2,23 @@
 #https://github.com/wharton/wrds
 # %% WRDS, need to install wrds package first
 import wrds
-# need username and password
+
+############################################
+### need username and password  ###
+############################################
 db = wrds.Connection(wrds_username='joe')
 db.create_pgpass_file()
-# %% read data
-params = {"tickers": ("AAPL", "WMT")}
+
+# %% read stock price and return data, AAPL's permno is 14593
+# %%
+apple = db.raw_sql("""select permno, date, prc, ret, shrout 
+                        from crsp.msf 
+                        where permno = 14593
+                        and date>='01/01/2019'""", 
+                     date_cols=['date'])# %% read financial data
+# %% read financial data
+params = {"tickers": ("AAPL")}
 financial_data = db.raw_sql(
     "SELECT tic, datadate, gvkey, cusip FROM comp.funda WHERE tic IN %(tickers)s",
     params=params,
 )
-# %% 
-stk_prc = db.raw_sql(
-    "SELECT * FROM crspa.crsp_a_stock WHERE ticker IN %(tickers)s ",
-    params=params,
-)
-# %%
