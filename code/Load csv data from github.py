8834355078fd19ff318d13f8ca0e942c68c07417
Cinder Zhang <72751738+CinderZhang@users.noreply.github.com>
2023-02-07 20:10:16 -0600

    Update Load csv data from github.py

--- code/Load csv data from github.py
+++ code/Load csv data from github.py
@@ -1,5 +1,5 @@
-# load csv data from github
+# %% load csv data from github
 import pandas as pd
 url = 'https://raw.githubusercontent.com/CinderZhang/DataAnalytics/main/data/AAPL_CRSP_Daily.csv'
 df = pd.read_csv(url)
-
+# %%
