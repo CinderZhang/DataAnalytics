db35f97b067e3b819cfa1efbcad3282ca0bc0f94
Cinder Zhang <72751738+CinderZhang@users.noreply.github.com>
2023-02-08 20:30:25 -0600

    Update WRDS.py

--- code/WRDS.py
+++ code/WRDS.py
@@ -2,22 +2,28 @@
 #https://github.com/wharton/wrds
 # %% WRDS, need to install wrds package first
 import wrds
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 ############################################
 ### need username and password  ###
 ############################################
-db = wrds.Connection(wrds_username='joe')
+db = wrds.Connection(wrds_username='cinderuark')
 db.create_pgpass_file()
 
 # %% read stock price and return data, AAPL's permno is 14593
 
-apple_stk = db.raw_sql("""select *
+apple_stk_monthly = db.raw_sql("""select *
                         from crsp.msf 
                         where permno = 14593
                         and date>='01/01/2019'""", 
                      date_cols=['date'])
 
-
+apple_stk_daily = db.raw_sql("""select *
+                        from crsp.dsf 
+                        where permno = 14593
+                        and date>='01/01/2019'""", 
+                     date_cols=['date'])
 
 # %% read financial data
 apple_fin = db.raw_sql("""select *
@@ -33,5 +39,13 @@ apple_fin = db.raw_sql("""select *
 #     "SELECT tic, datadate, gvkey, cusip,at FROM comp.funda WHERE tic IN %(tickers)s",
 #     params=params,
 # )
+
+# %% Fama French 5 factors
+ff_daily = db.raw_sql("""select *
+                        from ff.	fivefactors_daily 
+                        where date>='01/01/2019'""", 
+                     date_cols=['date'])
+# %% merge data ff_daily and apple_stk_daily
+apple_stk_ff_daily = pd.merge(apple_stk_daily, ff_daily, on='date', how='left')
 
 # %%
